b3571c3fecdd95fbc5f86e39c93edc60e9c53027
VedangS <vedangshrimankar@gmail.com>
2015-04-08 18:37:13 +0530

    Update Hello.java

--- Git_Test/src/com/git/test/Hello.java
+++ Git_Test/src/com/git/test/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello GIT.");
 		System.out.println("I Am Fine. ");
+		System.out.println("Hello Faurious ");
 	
 	}
 }
