4b725f94298a50a6d1244dc4fa16b098a3a12371
VedangS <vedang.shrimankar@uniscon.de>
2015-04-15 18:45:06 +0530

    Commit The changes

---
+++ Git_Test/src/com/git/test/Box.java
@@ -0,0 +1,13 @@
+package com.git.test;
+
+public class Hello {
+
+	/**
+	 * @This is Hello class displays Some messages.
+	 */
+	public static void main(String[] args) {
+		System.out.println("This file resides in a box file.");
+		
+	}
+}
+     
