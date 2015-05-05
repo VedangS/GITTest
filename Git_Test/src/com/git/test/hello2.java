ef8a972836ae852f9e11045080c1007585235976
VedangS <vedang.shrimankar@uniscon.de>
2015-05-05 16:38:24 +0530

    file Added.

---
+++ Git_Test/src/com/git/test/hello2.java
@@ -0,0 +1,18 @@
+package com.git.test;
+
+public class Hello {
+
+	/**
+	 * @This is Hello class displays Some messages.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello GIT.");
+		System.out.println("I Am Fine. ");
+		
+		
+		System.out.println("New Line Added ");
+
+		System.out.println("New Line Added gs ");
+	}
+}
+     
